cc95f3024b7b5dc47976cd14f18208d7eca0aa19
zclark0625 <zclark0625@gmail.com>
2020-04-25 00:03:00 -0400

    Added CORS configuration for heroku app, using CLIENT_ORIGIN env var

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import cors from "cors";
 import api from "./api";
 
 const port = process.env.PORT || 8080;
 const databaseURI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/tracker";
+
+if (process.env.CLIENT_ORIGIN) api.use(() => cors({origin: process.env.CLIENT_ORIGIN}));
 
 mongoose.connect(
     databaseURI,
